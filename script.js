08b9e5d95de900277ec1f9e8857d743fee18b15b
GaetanGras <51536105+GaetanGras@users.noreply.github.com>
2023-04-21 17:13:29 +0200

    changement titre
    mise à jour du titre

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-var TITLE = 'Air Transport, Passengers Carried (1970–2018)';
+var TITLE = 'Flux migratoire du Venezuela vers les Etats-Unis';
 
 // Caption underneath the chart
-var CAPTION = 'Source: The World Bank';
+var CAPTION = 'Source: HCR';
 
 // Add download link to the caption
 CAPTION += '<br><a href="https://github.com/HandsOnDataViz/highcharts-line-annotated" style="color:blue">View data and code</a>, \
